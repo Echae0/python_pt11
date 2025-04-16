1f16b46d78904617f11dca1469fc34b6f586853e
lamin5 <say8913@gmail.com>
2025-04-16 17:43:34 +0900

    streamlit 샘플

---
+++ streamlit_pt11.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+st.title("급성심장정지 위험도 파악 및 현황분석")
+
+region_name = st.text_input('조회할 지역이름과 연도를 선택해주세요 (예 : 경기, 인천)')
+button_clicked = st.button("조회")
+
+if button_clicked:
+    
