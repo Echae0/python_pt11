c18a5b684c7e20b8b0e32a76f6a9a0ca429cc8b1
lamin5 <say8913@gmail.com>
2025-04-16 18:18:08 +0900

    인증키 수정

--- streamlit_pt11.py
+++ streamlit_pt11.py
@@ -4,7 +4,4 @@ import pandas as pd
 st.title("급성심장정지 위험도 파악 및 현황분석")
 
 region_name = st.text_input('조회할 지역이름과 연도를 선택해주세요 (예 : 경기, 인천)')
-button_clicked = st.button("조회")
-
-if button_clicked:
-    
+button_clicked = st.button("조회")    
