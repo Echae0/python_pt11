255f286bb698635da5f996b4619c83cbc78edff6
lamin5 <say8913@gmail.com>
2025-04-18 10:11:33 +0900

    streamlit 추가

---
+++ steamlit_pt11_prec.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+# streamlit run streamlit_pt11_prec.py
+
+st.title("급성심장정지 위험도 파악 및 현황분석")
--- streamlit_pt11.py
+++ streamlit_pt11.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+# streamlit run streamlit_pt11.py
+
 
 st.title("급성심장정지 위험도 파악 및 현황분석")
 
