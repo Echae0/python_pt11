6ff95f7d293ec4a0d31f68bd14baaf2ead58f2c8
lamin5 <say8913@gmail.com>
2025-04-18 13:08:53 +0900

    예측 파일 추가

--- streamlit_pt11_prec.py
+++ streamlit_pt11_prec.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 # streamlit run streamlit_pt11_prec.py
 import os
 import numpy as np
+from python_pt11 import predicted_survival, predicted_value, model
+# UI
+st.title("📈 심정지 생존률 예측기")
 
-# model = joblib.load("survival_predictor.pkl")
+year = st.number_input("예측할 연도 (예: 2025)", min_value=2020, max_value=2100, value=2025)
 
-# # UI
-# st.title("📈 심정지 생존률 예측기")
+new_data = pd.DataFrame({'연도': [year]})
 
-# year = st.number_input("예측할 연도 (예: 2025)", min_value=2020, max_value=2100, value=2025)
 
-# if st.button("예측하기"):
-#     prediction = model.predict(np.array([[year]]))
-#     st.success(f"📊 {year}년 예상 생존률은 {prediction[0]:.2f}% 입니다.")
+print(f"2020년의 예측된 전체 생존율: {predicted_value[0]:.2f}")
+
+if st.button("예측하기"):
+    predicted_value = model.predict(new_data)
+    st.success(f"📊 {year}년 예상 생존률은 {predicted_value[0]:.2f}% 입니다.")
