d7f458d94e068bc26c78b8abffddf7705b15368f
lamin5 <say8913@gmail.com>
2025-04-21 09:28:59 +0900

    불필요 파일 제거

--- streamlit_pt11.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-# streamlit run streamlit_pt11.py
-
-st.title("급성심장정지 위험도 파악 및 현황분석")
-
-region_name = st.text_input('조회할 지역이름과 연도를 선택해주세요 (예 : 경기, 인천)')
-button_clicked = st.button("조회")
